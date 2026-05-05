e9ac93527c0a76ba8f4c5281ffbf5f18ec815c21
Elliana Perry <elliana.perry@gmail.com>
2026-05-05 16:35:30 -0400

    tweak: move directory initialization into init script

--- apps/postfix/scripts/entrypoint.sh
+++ apps/postfix/scripts/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Prepare required directories within PVC
+mkdir -p /var/spool/postfix 
+chown -R postfix:postfix /var/spool/postfix
+
 # Sanity check configuration before starting
 postfix check
 
